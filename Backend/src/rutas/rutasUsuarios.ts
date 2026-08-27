bf91bee2655ab6655258b93e8f65ad1434cc5d3c
matiasresnicoff-png <matias.resnicoff@gmail.com>
2026-08-27 13:26:26 -0300

    rutasu

--- Backend/src/rutas/rutasUsuarios.ts
+++ Backend/src/rutas/rutasUsuarios.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
-import { registrarUsuario } from '../controladores/usuariosControlador.ts';
+import { registrarUsuario, loginUsuario } from '../controladores/usuariosControlador.ts';
+
 const router = Router();
+
 router.post('/usuarios', registrarUsuario);
+router.post('/login', loginUsuario);
+
 export default router;
