629ef140f4ab4f300feb281c2dc637a40d244e93
matiasresnicoff-png <matias.resnicoff@gmail.com>
2026-08-27 13:45:24 -0300

    rutasusuario

--- Backend/src/rutas/rutasUsuarios.ts
+++ Backend/src/rutas/rutasUsuarios.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
-import { registrarUsuario, loginUsuario } from '../controladores/usuariosControlador.ts';
+import { registrarUsuario, loginUsuario, verPerfil, editarPerfil } from '../controladores/usuariosControlador.ts';
+import { verificarToken } from '../middlewares/verificarToken.ts';
+
 const router = Router();
+
 router.post('/usuarios', registrarUsuario);
 router.post('/login', loginUsuario);
+router.get('/usuarios/perfil', verificarToken, verPerfil);
+router.put('/usuarios/perfil', verificarToken, editarPerfil);
+
 export default router;
